9244ae3b66e1e30d4b466dc5af49b1afbb182e57
YOUR NAME <YOUR@EMAIL.com>
2020-01-07 02:57:08 +0900

    add stripe function(only pay)

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'post'
 urlpatterns = [
     path('about/', views.about, name='about'),
-    path('<slug:slug>/', views.post_detail, name='post_detail'),
+    #path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('<slug:slug>/', views.fee_detail, name='fee_detail'),
     path('', views.index, name='index'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
+import stripe
+from django.http import HttpResponse
 # Create your views here.
 def about(request):
     return HttpResponse('このブログサイトについて....')
@@ -12,3 +14,39 @@ def post_detail(request, slug):
 def index(request):
     posts = Post.objects.order_by('-created_datetime')
     return render(request, 'blog/index.html',{'posts':posts})
+
+def fee_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    #stripe
+    stripe.api_key = 'sk_test_L3ocIJYmmqbktHeyfNh9t9I600OpJsBvke'
+
+    if request.method == "POST":
+        try:
+            amount = 110   # amount in cents
+            customer = stripe.Customer.create(
+                email="customer@gamil.com",
+                source=request.POST['stripeToken']
+            )
+            print("stripe.Customer.create()     OK!")
+
+            stripe.Charge.create(
+                customer=customer.id,
+                amount=amount,
+                currency='jpy',
+                description='ブログのdjangoでStripe中....',
+                receipt_email="customer@gamil.com",
+            )
+            print("stripe.Charge.create()     OK!")
+            return HttpResponse("django blog で Stripe決済完了！")
+        except stripe.error.StripeError:
+            print("error......")
+
+    else:
+        sample_text = "Stripe上手くいくかな....."
+
+    return render(request, 'blog/fee_detail.html', {'post': post})
+
+    # first code
+    #post = get_object_or_404(Post, slug=slug)
+    #return render(request, 'blog/fee_detail.html', {'post':post})
+
